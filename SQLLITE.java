fc9cd5ab7a9424a731a125a9331d66395dc36366
Adriel Garcia Diaz <32568729+Adriel02@users.noreply.github.com>
2017-11-17 12:48:22 +0100

    Version 2

--- SQLLITE.java
+++ SQLLITE.java
@@ -1,20 +1,29 @@
 import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 
+import java.io.*;
 import java.sql.*;
 
 public class SQLLITE {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
         Class.forName("org.sqlite.JDBC");
         String urlConection="jdbc:sqlite:KATA5.DB";
         Connection connection= DriverManager.getConnection(urlConection);
 
         Statement statement = connection.createStatement();
-        String query="Select * from PEOPLE";
-        ResultSet rs = statement.executeQuery(query);
+        String query="CREATE TABLE IF NOT EXISTS MAIL ('Id' INTEGER PRIMARY KEY AUTOINCREMENT, 'Mail' TEXT NOT NULL);";
+        statement.execute(query);
 
-        while(rs.next()){
-            System.out.print(rs.getInt(1)+"-");
-            System.out.print(rs.getString(2)+" ");
+        String fileName ="C:\\Users\\ADRIELGARCÍADÍAZ\\IdeaProjects\\Kata5P1\\emails.txt";
+        BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)));
+        String mail;
+
+        while((mail=reader.readLine())!=null){
+            if(!mail.contains("@")) continue;
+            query="INSERT INTO MAIL (mail) VALUES ('" + mail +"')";
+            statement.executeUpdate(query);
         }
+
+        statement.close();
+        connection.close();
     }
 }
